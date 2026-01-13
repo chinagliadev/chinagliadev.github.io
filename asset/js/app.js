1e49e4e000dd7fc3f47258b9d17d6a0af0c42e8b
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2026-01-13 12:16:31 -0300

    animacao da section

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,10 +4,10 @@ document.querySelectorAll(".btn-ver-mais").forEach(btn => {
     btn.addEventListener("click", () => {
 
         const modalID = btn.dataset.modal;
-        if (!modalID) return; 
+        if (!modalID) return;
 
         const modal = document.getElementById(modalID);
-        if (!modal) return; 
+        if (!modal) return;
 
         modal.classList.remove("hide");
         modal.classList.add("show");
@@ -38,3 +38,40 @@ function fecharModal() {
 
     document.body.classList.remove("no-scroll");
 }
+
+const modal = document.getElementById("modal-studio-belle");
+const iframe = document.getElementById("studioBelleVideo");
+
+function abrirVideoStudioBelle() {
+     const videoId = "cshA86x3E2U";
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    modal.classList.remove("hide");
+}
+
+function fecharModalStudioBelle() {
+    iframe.src = "";
+    modal.classList.add("hide");
+}
+
+document.querySelector("[data-close]").addEventListener("click", fecharModalStudioBelle);
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            } else {
+                entry.target.classList.remove("active");
+            }
+        });
+    },
+    {
+        threshold: 0.2
+    }
+);
+
+reveals.forEach(reveal => observer.observe(reveal));
+
+
